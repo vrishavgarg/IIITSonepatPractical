0ef2bbfc4630e24c019960312e633de673351284
Vrishav Garg <92801805+vrishavgarg@users.noreply.github.com>
2022-02-25 09:33:55 +0530

    CP Assignment 1

--- DivisibiltyTest.c
+++ DivisibiltyTest.c
@@ -5,20 +5,57 @@ int main(){
     printf("Enter the natural number: "); 
     scanf("%d",&n);
 
-    if(n%2==0 && n%7==0 && n%11==0){
-        printf("The number %d is divisible by 2, 7 and 11",n);
-    }else if(n%2==0 && n%7==0){
-        printf("The number %d is divisible by 2 and 7",n);
-    }else if(n%2==0 && n%11==0){
-        printf("The number %d is divisible by 2 and 11",n);
-    }else if(n%11==0 && n%7==0){
-        printf("The number %d is divisible by 11 and 7",n);
-    }else if(n%2==0){
-        printf("The number %d is divisible by 2",n);
-    }else if(n%7==0){
-        printf("The number %d is divisible by 7",n);
-    }else{
-        printf("The number %d is divisible by 11",n);
+    switch(n%2){
+        case 0:
+            switch (n%7)
+            {
+            case 0:
+                switch (n%11){
+                    case 0:
+                       printf("The number %d is divisible by 2, 7 and 11",n);
+                       break;
+                    default:
+                        printf("The number %d is divisible by 2 and 7",n);
+                        break;
+                }
+                break;
+
+            default:
+                switch (n%11){
+                    case 0:
+                       printf("The number %d is divisible by 2 and 11",n);
+                       break;
+                    default:
+                        printf("The number %d is divisible by 2",n);
+                        break;
+                }
+                break;
+            }
+            break;
+        default:
+            switch (n%7)
+            {
+                case 0:
+                    switch (n%11){
+                    case 0:
+                       printf("The number %d is divisible by 7 and 11",n);
+                       break;
+                    default:
+                        printf("The number %d is divisible by 7",n);
+                        break;
+                }
+                break;
+
+                default:
+                    switch (n%11){
+                    case 0:
+                       printf("The number %d is divisible by 11",n);
+                       break;
+                    
+                }
+                break;
+            }
+            break;
     }
 
     return 0;
